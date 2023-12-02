7881c8d59ec7129a4f4f09fa4a47240c5d91e7bb
Riya Agrahari <122736924+riu2028@users.noreply.github.com>
2023-12-02 12:47:29 +0530

    Add files via upload

---
+++ numbers_pattern.php
@@ -0,0 +1,66 @@
+<?php
+for($i=1; $i<=5; $i++)
+{
+ for($j=$i; $j>=1; $j--)
+  echo $j % 2;
+ echo "<br>";
+}
+?>
+
+
+<?php
+echo"<br/><br/>";
+for($i=1; $i<=5; $i++)
+{
+ for($j=$i; $j<=5; $j++)
+  echo $j;
+ echo "<br>";
+}
+?>
+
+
+
+<?php
+echo"<br/><br/>";
+for($i=5; $i>=1; $i--)
+{
+ for($j=$i; $j>=1; $j--)
+  echo $j;
+ echo "<br>";
+}
+?>
+
+
+
+<?php
+echo"<br/><br/>";
+for($i=1; $i<=5; $i++)
+{
+ for($j=1; $j<=$i; $j++)
+  echo $j;
+ echo "<br>";
+}
+?>
+
+
+
+<?php
+echo"<br/><br/>";
+for($i=5; $i>=1; $i--)
+{
+ for($j=$i; $j<=5; $j++)
+  echo $j;
+ echo "<br>";
+}
+?>
+
+
+<?php
+echo"<br/><br/>";
+for($i=1; $i<=5; $i++)
+{
+ for($j=$i; $j>=1; $j--)
+  echo $j;
+ echo "<br>";
+}
+?>
